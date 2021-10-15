746049abb5dfa4475cbbaa1d4e037e7ce615cc6f
Britton Lorentzen <brittonlorentzen@gmail.com>
2021-10-15 16:48:48 -0700

    Adding paragraph node into example

--- files/scripts/app.js
+++ files/scripts/app.js
@@ -9,6 +9,7 @@ window.onload = function() {
 	
 	// Add a simple headline element
 	testContainer.addElement(createHeadline('h1', 'Hello World, EmpacJS is on the way.'));
+	testContainer.addElement(createParagraph('This is an experiment space at the moment. More will come eventually.'));
 	
 	// Append to body
 	document.body.append(testContainer.returnMarkup());

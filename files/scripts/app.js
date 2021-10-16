09e2fc3baa2238500e1ef49ae42c7a72a5eb1e25
Britton Lorentzen <brittonlorentzen@gmail.com>
2021-10-15 20:30:59 -0700

    Add in tests for adding a button inside a container

--- files/scripts/app.js
+++ files/scripts/app.js
@@ -7,9 +7,14 @@ window.onload = function() {
 	testContainer.setID('helloWorld');
 	testContainer.addClass('helloWorld');
 	
+	// Set up basic button
+	const testButton = new Button();
+	testButton.setText('Test Button');
+	
 	// Add a simple headline element
 	testContainer.addElement(createHeadline('h1', 'Hello World, EmpacJS is on the way.'));
 	testContainer.addElement(createParagraph('This is an experiment space at the moment. More will come eventually.'));
+	testContainer.addElement(testButton.returnMarkup());
 	
 	// Append to body
 	document.body.append(testContainer.returnMarkup());

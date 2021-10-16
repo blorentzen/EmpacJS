93bf3406d0160c8dc04efc9df59c62b13e6b4a4a
Britton Lorentzen <brittonlorentzen@gmail.com>
2021-10-16 10:14:36 -0700

    Update application with test link

--- files/scripts/app.js
+++ files/scripts/app.js
@@ -8,13 +8,14 @@ window.onload = function() {
 	testContainer.addClass('helloWorld');
 	
 	// Set up basic button
-	const testButton = new Button();
-	testButton.setText('Test Button');
+	const testButton = new Button('Test Button');
+	const testLink = new Link('#', 'Test Link');
 	
 	// Add a simple headline element
 	testContainer.addElement(createHeadline('h1', 'Hello World, EmpacJS is on the way.'));
 	testContainer.addElement(createParagraph('This is an experiment space at the moment. More will come eventually.'));
 	testContainer.addElement(testButton.returnMarkup());
+	testContainer.addElement(testLink.returnMarkup());
 	
 	// Append to body
 	document.body.append(testContainer.returnMarkup());

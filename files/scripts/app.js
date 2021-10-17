1a3b22a585e93328eab3210e8203581ebffadea0
Britton Lorentzen <brittonlorentzen@gmail.com>
2021-10-17 14:20:09 -0700

    Simplify the main application to create a hero and export to page

--- files/scripts/app.js
+++ files/scripts/app.js
@@ -2,21 +2,10 @@
 
 window.onload = function() {
 	
-	// Set up basic container
-	const testContainer = new Container();
-	testContainer.setID('helloWorld');
-	testContainer.addClass('helloWorld');
-	
-	// Set up basic button
-	const testButton = new Button('Test Button');
-	const testLink = new Link('#', 'Test Link');
-	
-	// Add a simple headline element
-	testContainer.addElement(createHeadline('h1', 'Hello World, EmpacJS is on the way.'));
-	testContainer.addElement(createParagraph('This is an experiment space at the moment. More will come eventually.'));
-	testContainer.addElement(testButton.returnMarkup());
-	testContainer.addElement(testLink.returnMarkup());
+	// Set up basic hero
+	const testModule = new Hero();
 	
 	// Append to body
-	document.body.append(testContainer.returnMarkup());
+	document.body.append(testModule.returnMarkup());
+
 }

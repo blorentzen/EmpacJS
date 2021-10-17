ae89197a3904c0f296b3b622be9df7a8198149b3
Britton Lorentzen <brittonlorentzen@gmail.com>
2021-10-17 14:29:49 -0700

    Add multi-column component for testing

--- files/scripts/app.js
+++ files/scripts/app.js
@@ -5,7 +5,11 @@ window.onload = function() {
 	// Set up basic hero
 	const testModule = new Hero();
 	
+	// Set up testing multicolumn
+	const testMulti = new MultiColumn(3);
+	
 	// Append to body
 	document.body.append(testModule.returnMarkup());
+	document.body.append(testMulti.returnMarkup());
 
 }
